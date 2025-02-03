c4ed64036523f288bf3e4c4e4fb6f94a00661c32
Shaharukh Khan <shaharukh.khan@C17GF5N1Q6L4.local>
2025-02-03 18:56:49 +0530

    removed openAI calls

--- chitrarth/utils.py
+++ chitrarth/utils.py
@@ -99,27 +99,6 @@ def disable_torch_init():
     setattr(torch.nn.LayerNorm, "reset_parameters", lambda self: None)
 
 
-def violates_moderation(text):
-    """
-    Check whether the text violates OpenAI moderation API.
-    """
-    url = "https://api.openai.com/v1/moderations"
-    headers = {"Content-Type": "application/json",
-               "Authorization": "Bearer " + os.environ["OPENAI_API_KEY"]}
-    text = text.replace("\n", "")
-    data = "{" + '"input": ' + f'"{text}"' + "}"
-    data = data.encode("utf-8")
-    try:
-        ret = requests.post(url, headers=headers, data=data, timeout=5)
-        flagged = ret.json()["results"][0]["flagged"]
-    except requests.exceptions.RequestException as e:
-        flagged = False
-    except KeyError as e:
-        flagged = False
-
-    return flagged
-
-
 def pretty_print_semaphore(semaphore):
     if semaphore is None:
         return "None"
